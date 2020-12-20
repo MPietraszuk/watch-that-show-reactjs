9378354118b224e8c0c7d89b0a41442daa921122
mpietraszuk <GitHub@MarkZDesign.com>
2020-12-19 16:01:55 -0800

    Routes10

--- src/components/App.js
+++ src/components/App.js
@@ -3,20 +3,21 @@ import Header from './Header';
 import Home from './Home';
 import MovieCard from './MovieCard';
 import MovieList from './MovieList';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 class App extends Component {
   state = { message: 'Yoo, React !!!!' }
   render() {
     return (
-      <div>
-        <Header />
-        {/* <Home /> */}
-        {/* <MovieCard /> */}
-        <div className="container">
-          <MovieList />
+      <BrowserRouter>
+        <div>
+          <Header />
+          <div className="container">
+            <Route path="/" exact={true} component={Home} />
+            <Route path="/movies" component={MovieList} />
+          </div>
         </div>
-
-      </div>
+      </BrowserRouter>
     );
   }
 }
