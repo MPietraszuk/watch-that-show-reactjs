cdcbc529339f39557228d15ab8521d3bfaa82de7
mpietraszuk <GitHub@MarkZDesign.com>
2020-12-19 16:14:29 -0800

    QueryStringParameters11

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,17 +1,19 @@
 import Axios from 'axios';
 import React, { Component } from 'react';
 import MovieCard from './MovieCard';
+import queryString from 'query-string';
 
 class MovieList extends Component {
     state = { movies: [] }
 
     componentDidMount() {
         this.fetchMovies();
+        console.log(this.props);
     }
 
     fetchMovies = () => {
-        let searchText = "Bruce Lee";
-        Axios.get(`http://www.omdbapi.com/?apikey=b51ec88b&s=${searchText}`)
+        let q = queryString.parse(this.props.location.search);
+        Axios.get(`http://www.omdbapi.com/?apikey=b51ec88b&s=${q.searchText}`)
         .then(resp=>resp.data)
         .then(data=>data.Search)
         .then(movies=>this.setState({movies}));
